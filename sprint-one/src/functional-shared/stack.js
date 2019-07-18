c4cd822b29bc08446f8afc9d5380042c7ef59ce9
Daniel Kang <danieldonghoonkang@gmail.com>
2019-07-18 13:30:41 -0700

    completed

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.num = 0;
+  extend (someInstance, stackMethods)
+  return someInstance;
+};
+
+var extend = function (to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+    this.storage[this.num] = value;
+    this.num++;
+  };
+stackMethods.pop = function() {
+    var temp = this.storage[this.num - 1];
+    delete this.storage[this.num - 1];
+    this.num--;
+    return temp;
+  };
+  stackMethods.size = function() {
+    if (this.num < 0) {
+      return 0;
+    }
+    return this.num;
+  };
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var storageNumFirst = 0;
+  var storageNumLast = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[storageNumLast] = value;
+    storageNumLast++;
   };
 
-  someInstance.dequeue = function() {
+  someInstance.dequeue = function() { // [a,b *removes a*, c, *---NOW REMOVE B--]
+    //
+    var temp = storage[storageNumFirst];
+    delete storage[storageNumFirst];
+    storageNumFirst++;
+    return temp;
   };
 
   someInstance.size = function() {
+    if (storageNumLast - storageNumFirst < 0) {
+      return 0;
+    }
+    return storageNumLast - storageNumFirst;
   };
 
   return someInstance;
