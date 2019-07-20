d0cbbd5a3ddae984a2cf1579c8d92e6460fc014c
DDKangz <danieldonghoonkang@gmail.com>
2019-07-19 21:06:16 -0700

    started binary tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,27 @@
 var BinarySearchTree = function(value) {
+  var newBSTree = {};
+  newBSTree.value = value;
+
+  newBSTree.left = null;
+  newBSTree.right = null;
+
+  newBSTree.insert = function(value) {
+
+  };
+
+  newBSTree.contains = function(value) {
+
+  };
+
+  newBSTree.depthFirstLog = function(value) {
+
+  };
+
+  return newBSTree;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
