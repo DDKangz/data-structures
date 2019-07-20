c81a4852d6252b9f829968abc4aafd6be8db6910
DDKangz <danieldonghoonkang@gmail.com>
2019-07-20 14:57:53 -0700

    completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this[item] = null;
 };
 
 setPrototype.contains = function(item) {
+  if(this[item] !== undefined) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  delete this[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+ 
